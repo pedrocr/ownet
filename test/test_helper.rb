679abefc08992b708f805b935cc4b6adb9b17b09
Pedro Côrte-Real <pedro@pedrocr.net>
2010-09-17 15:31:30 +0100

    Test to see if owserver is installed.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require File.dirname(__FILE__)+'/../lib/ownet.rb'
 
 class Test::Unit::TestCase  
   def with_fake_owserver
+    assert(system("owserver --version > /dev/null 2>&1"), 
+           "owserver not installed")
+
     pid = fork do 
       exec("owserver", "--fake", "1F,10", "--foreground")
     end
